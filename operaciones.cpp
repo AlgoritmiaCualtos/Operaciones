f104d69375a141c409f73420d752d3efba8d92aa
sergio-jose-aguas-montano <71527413+sergio-jose-aguas-montano@users.noreply.github.com>
2020-10-03 23:03:14 -0500

    terminado

---
+++ operaciones.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("La suma, resta, multiplicacion y division de dos numeros es:\n");
+
+    int x;
+    int y;
+    int resultado;
+    double cociente;
+
+    printf("Ingresa el primer numero: \n");
+    scanf("%d",&x);
+
+    printf("Ingresa el segundo numero: \n");
+    scanf("%d",&y);
+
+    resultado = x + y;
+
+    printf("El resultado de la suma es: %d,\n",resultado);
+
+    resultado = x - y;
+
+    printf("El resultado de la resta es: %d,\n",resultado);
+
+    resultado = x * y;
+
+    printf("El resultado de la multiplicacion es: %d,\n",resultado);
+
+    cociente = x / y;
+
+    printf("El resultado de la division es: %lf,\n",cociente);
+
+return 0;
+}
